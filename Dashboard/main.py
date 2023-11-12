ff519902cbf0bca25543c6cafb3df2b76f21546b
SimonGrishin <124309228+SimonGrishin@users.noreply.github.com>
2023-11-12 07:31:15 -0500

    Updated main

--- Dashboard/main.py
+++ Dashboard/main.py
@@ -3,10 +3,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import time
 from PIL import Image
+from io import BytesIO
 
-def refresh_button():
-    if st.button("Refresh Page"):
-        st.experimental_rerun()
+
+#st.set_config_file(path="./.streamlit/config.toml")
+
 
 # from streamlit_autorefresh import st_autorefresh
 
@@ -14,6 +15,13 @@ def refresh_button():
 
 
 plant_image = 'farm_corn.jpg'
+crop = "Corn"
+percentage_crop = 85
+percentage_disease = 90
+new_t = 33
+new_h = 68
+sun_light = ["Full Sun", "Part Shade", "Full Shade"]
+moisture = 10
 
 header = st.container()
 dataset = st.container()
@@ -22,7 +30,7 @@ model_training = st.container()
 sample_data = pd.DataFrame({"Temperature": [24, 25, 27, 28, 28, 28, 28, 29, 30 ,30 ,29, 30 ,30 ,30, 32],
                             "Humidity": [65, 70, 69, 66, 66, 66, 66, 66, 65, 64, 66, 63, 62, 66, 65]})
 
-crop = "Corn"
+
 
 df = pd.read_csv('HackPrinceton Plant Data.csv')
 low_temp, high_temp = df[df['plant_name'] == crop]['temp_low'].values[0], df[df['plant_name'] == 'Corn']['temp_high'].values[0]
@@ -36,22 +44,45 @@ count_val = lambda x: (1 if x > high_h else 1 if x < low_h else 0)
 
 count_h = list(map(count_val, sample_data['Humidity'])).count(1)
 
+#Select Box
+add_selectbox = st.sidebar.selectbox(
+    "Which Module would you like to access?",
+    ("Apple_1", "Apple_2", "Tomato_1", "Tomato_3", "Corn_3", "Corn_1")
+)
+
+# Using "with" notation
+with st.sidebar:
+    if add_selectbox == 'Corn_1':
+        with st.spinner("Please wait..."):
+            time.sleep(3)
+    else:
+        st.write("Module Disconnected")
+
+
 
 with header:
-    st.title('Welcome to HackPrinceton!')
-    refresh_button()
+    # st.title("Harvest Hero")
+    st.markdown(f"""# Harvest Hero""")
 
 
-# Select Box
-# add_selectbox = st.sidebar.selectbox(
-#     "How would you like to be contacted?",
-#     ("Email", "Home phone", "Mobile phone")
-# )
+    if st.button('Run Data Collection', use_container_width=True):
+        with st.spinner('Please wait...'):
+            time.sleep(2)
+        #input function to run 
+            
+        st.experimental_rerun()
 
-# # Using "with" notation
-# with st.sidebar:
-#     if add_selectbox == 'Home phone':
-#         st.write('Thank you for choosing')
+
+
+#     place.markdown("""
+# <div style="position: absolute; top: 0px; left: 250px;">
+#     <h1>Harvest Hero</h1>
+# </div>
+# """, unsafe_allow_html=True)
+    # st.title("Harvest Hero", anchor='center')
+
+
+
 
 
 
@@ -60,32 +91,39 @@ with dataset:
     col1, col2 = st.columns(2)
    # Columns 
     with col1: 
-        st.subheader('Plant Optimal Dataset')
+        def plotChart():
+            subheader_text = "Plant Optimal Data"
+            centered_subheader = f"<h3 style='text-align: center;'>{subheader_text}</h3>"
+            st.markdown(centered_subheader, unsafe_allow_html=True)
 
-        fig, ax1 = plt.subplots()
-        # Plotting temperature
+            fig, ax1 = plt.subplots()
+            # Plotting temperature
 
-        ax1.plot(sample_data['Temperature'], marker='o', color=[0.18, 0.32, 0.70], label='Temperature')
-        ax1.axhspan(low_temp, high_temp, facecolor=[0.248, 0.42, 0.692], alpha=0.3, label='Ideal Temperature')
-        ax1.set_ylabel('Temperature (°C)').set_color([0.248, 0.42, 0.692])
-        ax1.set_ylim(0, 100)
-        plt.title('Temperature and Humidity Data')
+            ax1.plot(sample_data['Temperature'], marker='o', color=[0.18, 0.32, 0.70], label='Temperature')
+            ax1.axhspan(low_temp, high_temp, facecolor=[0.248, 0.42, 0.692], alpha=0.3, label='Ideal Temperature')
+            ax1.set_ylabel('Temperature (°C)').set_color([0.248, 0.42, 0.692])
+            ax1.set_ylim(0, 100)
+            plt.title('Temperature and Humidity Data')
 
-        # Plotting humidity
-        ax2 = ax1.twinx()
-        ax2.set_ylim(0,100)
-        ax2.plot(sample_data['Humidity'], marker='o', color=[0.148,0.42, 0.16], label='Humidity')
-        ax2.axhspan(low_h, high_h, facecolor='green', alpha=0.3, label='Ideal Humidity')
-        ax2.set_ylabel('Humidity (%)').set_color('green')
-        # [0.828, 0.58, 0.156]
-        fig.legend()
-        ax1.grid(axis='y')
+            # Plotting humidity
+            ax2 = ax1.twinx()
+            ax2.set_ylim(0,100)
+            ax2.plot(sample_data['Humidity'], marker='o', color=[0.148,0.42, 0.16], label='Humidity')
+            ax2.axhspan(low_h, high_h, facecolor='green', alpha=0.3, label='Ideal Humidity')
+            ax2.set_ylabel('Humidity (%)').set_color('green')
+            # [0.828, 0.58, 0.156]
+            fig.legend()
+            ax1.grid(axis='y')
 
-        # Adjust layout
-        fig.tight_layout()
+            # Adjust layout
+            fig.tight_layout()
 
-        # Show the plot
-        st.pyplot(fig)
+            # Show the plot
+            st.pyplot(fig)
+
+        sample_data.loc[len(sample_data)] = [new_t, new_h]
+        plotChart()
+
 
 
         # Dialog boxes for temperature and Humidity readings 
@@ -99,43 +137,39 @@ with dataset:
 
     with col2:
         # Image data
-        st.subheader("Image Data", divider=[])
+        # Define a subheader
+        subheader_text = "Image Data"
+        centered_subheader = f"<h3 style='text-align: center;'>{subheader_text}</h3>"
+        st.markdown(centered_subheader, unsafe_allow_html=True)
+
+
+        
         def load_image():
-            st.markdown(
-                """
-                <div style="display: flex; justify-content: space-between; align-items: center;">
-                    <div style="flex: 1; margin-right: 20px;">
-                        <img src="//farm_corn.jpg" alt="Image 1" style="width: 100%;">
-                        <p style="text-align: center;">Caption for Image 1</p>
-                    </div>
-                </div>
-                """,
-                unsafe_allow_html=True
-            )
 
             image = Image.open(plant_image)
-            st.image(image, caption=str(crop))
+            st.image(image, caption=str(crop), width=400)
 
         load_image()
+        st.info(f"""**Crop Classification Accuracy:** {percentage_crop}%""")
+        # st.markdown(f"""**Crop Classification Accuracy: {percentage_crop}**%""")
+        st.info(f"""**Disease Classification Accuracy:** {percentage_disease}%""")
         ## Place Holder
 
 
-        if st.button('Reload'):
+        if st.button('Reload', use_container_width=True):
             with st.spinner('Loading Image...'):
-                time.sleep(3)
+                time.sleep(2)
 
 
 
     
 
     with features:
-        st.header('The features I created')
+        st.markdown(f"""### <u>Soil Moisure and Sun Light data:</u>""", unsafe_allow_html=True)
 
-    with model_training:
-        st.header('Model_training')
-        st.subheader('Crop Classification')
-        st.text('Determine the plant species: ')
-
-        st.subheader('Crop disease and Pest classification')
-        st.text('Determine if any diseases or pests have infected the crop')
+        
+        col1, col2 = st.columns(2)
+        col1.metric("Soil Moisture", "{m}%".format(m = moisture), "1.2 °F")
+        col2.metric("Sun","{light}".format(light = sun_light[0]) , "-8%")
+        
     
