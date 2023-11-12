1699cd73d5d811bf4110eec529b2713d0348c67c
Aaron-Lozhkin <Aaronlozhkin@gmail.com>
2023-11-12 01:31:43 -0500

    Added image support for raspberry PI module

--- RaspberryPi2Azure.py
+++ RaspberryPi2Azure.py
@@ -1,5 +1,7 @@
 import random
 import time
+import cv2
+import base64
 
 from azure.iot.device import IoTHubDeviceClient, Message
 
@@ -7,7 +9,7 @@ CONNECTION_STRING = "HostName=CropManagementSystem.azure-devices.net;DeviceId=Ra
 
 TEMPERATURE = 20.0
 HUMIDITY = 60
-MSG_TXT = '{{"temperature": {temperature},"humidity": {humidity}}}'
+MSG_TXT = '{{"temperature": {temperature},"humidity": {humidity}, "image":{image}}}'
 
 def iothub_client_init():
     # Create an IoT Hub client
@@ -22,7 +24,16 @@ def iothub_client_telemetry_sample_run():
             # Build the message with simulated telemetry values.
             temperature = TEMPERATURE + (random.random() * 15)
             humidity = HUMIDITY + (random.random() * 20)
-            msg_txt_formatted = MSG_TXT.format(temperature=temperature, humidity=humidity)
+
+            image = cv2.VideoCapture(0)
+            ret, frame = image.read()
+            image.release()
+
+            resized_image = cv2.resize(frame, (224, 224))
+            image = cv2.imencode(".jpg", resized_image)[1]
+            image = base64.b64encode(image).decode("utf-8")
+
+            msg_txt_formatted = MSG_TXT.format(temperature=temperature, humidity=humidity, image=image)
             message = Message(msg_txt_formatted)
             # Add a custom application property to the message.
             # An IoT hub can filter on these properties without access to the message body.
