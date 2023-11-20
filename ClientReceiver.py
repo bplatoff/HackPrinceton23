3b952548fa73854e51917ad785c0c388dc532bff
Aaron-Lozhkin <Aaronlozhkin@gmail.com>
2023-11-19 21:01:04 -0500

    Changed everything to classes

--- ClientReceiver.py
+++ ClientReceiver.py
@@ -11,134 +11,109 @@ import cv2
 import torch
 import torch.nn.functional as F
 from torchvision import transforms
-
 from DiseaseClassifier import ResNetClassifer
 
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-if not torch.cuda.is_available(): print("WARNING: Model will run extremely slow on cpu")
-
-# Load the model
-DiseasePredictor = ResNetClassifer(38).to(device)
-
-if torch.cuda.is_available():
-    DiseasePredictor.load_state_dict(torch.load("DiseasedCropClassifier_5epochs_secondIteration.pth"))
-else:
-    print("WARNING: Model will run extremely slow on cpu. If on colab, go to Runtime->Change Runtime Type->Hardware Accelerator->GPU.")
-    DiseasePredictor.load_state_dict(torch.load("/content/siamese_triplet_model_cache.pth", map_location=torch.device('cpu')))
-DiseasePredictor.eval()
-
-transform = transforms.Compose([
-    transforms.ToPILImage(),
-    transforms.Resize((224, 224)),  # Assuming your model expects images of size (224, 224)
-    transforms.ToTensor(),
-])
-
-predictions = []
-start_time = time.time()
-change_camera_angle_condition = False
-
-
-# Set up socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("192.168.1.168", 5010))
-while True:
-
-    with open('Test Images/switch_camera.txt', 'rb') as file:
-        is_camera_change = False
-
-    print(is_camera_change)
-
-    if is_camera_change:
-        start_time = time.time()
-        # Add your code to change the camera angle here
-
-        # Send a message back to the server indicating the camera angle change
-        message = "camera_angle_changed"
-        print("Sending Camera Change message")
-    else:
-        message = "continue"
-    s.sendall(message.encode("utf-8"))
-
-    # Receive the data length
-    data_len_bytes = s.recv(struct.calcsize("Q"))
-    if len(data_len_bytes) < struct.calcsize("Q"):
-        print("Incomplete data length received")
-    else:
-        data_len = struct.unpack("Q", data_len_bytes)[0]
-        print("Received Data of Length: ", data_len)
-
-        # Receive the data
-        data = b""
-        while len(data) < data_len:
-            packet = s.recv(data_len - len(data))
-            if not packet:
-                print("Connection closed or packet is empty")
-                break
-            data += packet
-
-        if len(data) == data_len:
-            try:
-                # Deserialize the received data from JSON
-                received_data = json.loads(data.decode())
-
-                # Extract the frame and additional parameters
-                jpg_as_text = received_data["image"]
-                additional_parameters = received_data["params"]
-
-                # Convert the base64 string back to numpy array
-                if jpg_as_text != 0:
-                    jpg_original = base64.b64decode(jpg_as_text)
-                    jpg_as_np = np.frombuffer(jpg_original, dtype=np.uint8)
-                    frame = cv2.imdecode(jpg_as_np, flags=1)
-
-                    # Further processing with frame...
-                else:
-                    frame = None
-                    # Further processing without frame...
-
-            except json.JSONDecodeError as e:
-                print(f"JSONDecodeError: {e}")
-        else:
-            print("Incomplete data received")
-            
-    disease_status = "Healthy"
-    predicted_probability = 67.345
-
-    if frame is not None:
-        print("Applying Deep Learning Model")
-        # rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        cv2.imwrite('Test Images/CurrentImage.jpg',frame)
-        input_tensor = transform(frame)
-        input_tensor = input_tensor.unsqueeze(0)  # Add batch dimension
-
-        # Move the input tensor to the same device as the model
-        input_tensor = input_tensor.to(device)
-
-        # Forward pass through the model
-        with torch.no_grad():
-            prediction = DiseasePredictor(input_tensor)
+class ReceiveServer():
+    def __init__(self, IP, PORT):
         
-        probabilities = F.softmax(prediction, dim=1)
-        predicted_probability, predicted_classes = probabilities.max(1)
-        predicted_classes = predicted_classes.item()
-        plant_type, disease_status = DiseasePredictor.idx_to_class[predicted_classes]
-        predicted_probability = predicted_probability.item() * 100
+        # Load the Model
+        self.DiseasePredictor = ResNetClassifer(38)
+        self.DiseasePredictor.loadModel()
 
+        self.is_camera_change = False
 
-        # Store the prediction in the list
-        predictions.append((plant_type, disease_status, predicted_probability))
+        # Set default values in case of failed receiving
+        self.start_time = time.time()
+        self.plant_type = "Corn"
+        self.disease_status = "Healthy"
+        self.predicted_probability = 67.345
+        self.data = []
 
-    print("Received Parameters: ", additional_parameters)
-    data = [disease_status, predicted_probability, additional_parameters['Temperature'], additional_parameters['Humidity'], additional_parameters['Light Value'], additional_parameters['Moisture Value']]
-    file = open('Test Images/newdata.txt', 'wb')
+        # Set up socket
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((IP, PORT))
 
-    # dump information to that file
-    pickle.dump(data, file)
+    def sendMessage(self):
+        # Sends a True/False message depending on if we want to rotate the camera
 
-    # close the file
-    file.close()
+        try:
+            with open('Test Images/switch_camera.txt', 'rb') as file:
+                self.is_camera_change = pickle.load(file)[0]
+        except Exception as e:
+            with open('Test Images/switch_camera.txt', 'wb') as file:
+                pickle.dump(self.is_camera_change, file)
 
-# except Exception as error:
-#     print(error)
-#     print("Something is going wrong :(")
+        print(self.is_camera_change)
+
+        if self.is_camera_change:
+            # Send a message back to the server indicating the camera angle change
+            message = "camera_angle_changed"
+            print("Sending Camera Change message")
+        else:
+            message = "continue"
+
+        self.socket.sendall(message.encode("utf-8"))
+
+    def receiveMessage(self):
+
+        # Receive the data length
+        data_len_bytes = self.socket.recv(struct.calcsize("Q"))
+        if len(data_len_bytes) < struct.calcsize("Q"):
+            print("Incomplete data length received")
+        else:
+            data_len = struct.unpack("Q", data_len_bytes)[0]
+            print("Received Data of Length: ", data_len)
+
+            # Receive the data
+            data = b""
+            while len(data) < data_len:
+                packet = self.socket.recv(data_len - len(data))
+                if not packet:
+                    print("Connection closed or packet is empty")
+                    break
+                data += packet
+
+            if len(data) == data_len:
+                try:
+                    # Deserialize the received data from JSON
+                    received_data = json.loads(data.decode())
+
+                    # Extract the frame and additional parameters
+                    jpg_as_text = received_data["image"]
+                    additional_parameters = received_data["params"]
+
+                    # Convert the base64 string back to numpy array
+                    if jpg_as_text != 0:
+                        jpg_original = base64.b64decode(jpg_as_text)
+                        jpg_as_np = np.frombuffer(jpg_original, dtype=np.uint8)
+                        frame = cv2.imdecode(jpg_as_np, flags=1)
+                    else:
+                        frame = None
+
+                except json.JSONDecodeError as e:
+                    print(f"JSONDecodeError: {e}")
+            else:
+                print("Incomplete data received")
+
+        if frame is not None:
+            print("Applying Deep Learning Model")
+            cv2.imwrite('Test Images/CurrentImage.jpg', frame)
+
+            plant_type, disease_status, predicted_probability = self.DiseasePredictor.apply(frame)
+
+        print("Received Parameters: ", additional_parameters)
+        self.data = [plant_type, disease_status, predicted_probability, additional_parameters['Temperature'], additional_parameters['Humidity'], additional_parameters['Light Value'], additional_parameters['Moisture Value']]
+
+        # Dump the new data into the text file
+        with open('Test Images/newdata.txt', 'wb') as file:
+            pickle.dump(data, file)
+
+    def runServer(self):
+        while True:
+            self.sendMessage()
+            self.receiveMessage()
+            time.sleep(.1)
+
+IP, PORT = "192.168.1.158", 5010
+clientServer = ReceiveServer(IP, PORT)
+clientServer.runServer()
--- RaspberryPiServer.py
+++ RaspberryPiServer.py
@@ -1,101 +1,123 @@
-#!/usr/bin/env python3
 import signal
 import sys
 import json
-import serial
 import base64
 import socket
 import time
 from threading import Timer
-import cv2
-import pickle
 import struct
+import random
+import serial
+import cv2
 
-#eecutes when command+z is sent
-def sigint_handler(signal, frame):
-	print(' Interrupted')
-	s.close()
-	sys.exit(0)
+class TransmitServer():
+	def __init__(self, IP, PORT, emulate_data=False):
+		self.IP = IP
+		self.PORT = PORT
+		self.emulate = emulate_data
 
-signal.signal(signal.SIGTSTP, sigint_handler)
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('',5010))
-s.listen(5)
-ser = None
-print("Server is now running")
-
-def background_controller():
-	
-	params = {"Humidity":"0", "Temperature":"0", "Moisture Value": "0", "Light Value": "0"}
-	
-	while ser.in_waiting > 0:
-		#print("Line recieved")
-		line = ser.readline().decode('utf-8').rstrip().split(':')
-		line = [l.strip() for l in line]
-		print(line)
-		if line[0] in params.keys():
-			params[line[0]] = line[1]
-
-	# implement check to see if we should be taking a photo
-	take_photo = True
-
-	try:
-		message = clientsocket.recv(1024).decode("utf-8")
-		if message == "camera_angle_changed":
-			take_photo = False
-			print("Change the Camera!")
-			ser.reset_input_buffer()
-			ser.write(b"turn\n")
-			while ser.in_waiting == 0:
-				time.sleep(.5)
-			line = ser.readline().decode('utf-8').rstrip()
-			if line == "turn_complete":
-				take_photo = True
-			
+		self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.server_socket.bind((IP, PORT))
+		self.server_socket.listen(5)
+		self.client_socket = None
+		self.serial_conn = None
+		self.setup_signal_handler()
+		print("Server is now running")
 		
-	except socket.error as e:
-		pass # No message received, ignore the error
+		self.sensor_data = {"Humidity": "0", "Temperature": "0", "Moisture Value": "0", "Light Value": "0"}
 
-	if take_photo:
-		print("Capturing Image")
+		if not emulate_data:
+			self.serial_conn = serial.Serial('/dev/tty.usbserial-0001', 9600, timeout=1)
+			self.serial_conn.reset_input_buffer()
+
+	def setup_signal_handler(self):
+		signal.signal(signal.SIGINT, self.sigint_handler)
+
+	def sigint_handler(self, signal, frame):
+		print('Interrupted')
+		if self.client_socket:
+			self.client_socket.close()
+		self.server_socket.close()
+		sys.exit(0)
+
+	def read_sensor_data(self):
+		
+		if self.emulate:
+			# Generate Fake Data
+			self.sensor_data = {"Humidity": str(round(random.uniform(20, 80), 2)), "Temperature": str(round(random.uniform(20, 80), 2)),
+					   			"Moisture Value": str(round(random.uniform(20, 80), 2)), "Light Value": str(round(random.uniform(0, 100)))}
+		else:
+			while self.serial_conn.in_waiting > 0:
+				line = self.serial_conn.readline().decode('utf-8').rstrip().split(':')
+				line = [l.strip() for l in line]
+				if line[0] in self.sensor_data.keys():
+					self.sensor_data[line[0]] = line[1]
+		
+			
+	def receive_data(self):
+
+		take_photo = True
+
+		try:
+			message = self.client_socket.recv(1024).decode("utf-8")
+			if message == "camera_angle_changed":
+
+				if self.emulate:
+					print("Received Turn Message")
+				else:
+					take_photo = False
+					self.serial_conn.reset_input_buffer()
+					self.serial_conn.write(b"turn\n")
+					while self.serial_conn.in_waiting == 0:
+						time.sleep(.5)
+					line = self.serial_conn.readline().decode('utf-8').rstrip()
+					if line == "turn_complete":
+						take_photo = True
+		except socket.error as e:
+			pass  # No message received, ignore the error
+
+		if take_photo:
+			self.send_data()
+		Timer(5, self.receive_data).start()
+		
+	def capture_image(self):
+
 		camera = cv2.VideoCapture(0)
-		ret, frame = camera.read()
+		_, frame = camera.read()
 		camera.release()
 
-		# Encode the frame in JPEG format
-		retval, buffer = cv2.imencode('.jpg', frame)
-		
-		# Convert to base64 encoding and decode to string
+		_, buffer = cv2.imencode('.jpg', frame)
 		jpg_as_text = base64.b64encode(buffer).decode()
 
-		# Prepare data as a dictionary
-		data = {"image": jpg_as_text, "params": params}
-	else:
-		data = {"image": 0, "params": params}
-        
-	# Serialize to JSON
-	json_data = json.dumps(data)
+		if frame is not None: 
+			return jpg_as_text 
+		return 0
+			
 
-	# Convert to bytes
-	bytes_data = json_data.encode()
-
-	try:
-		# print(bytes_data)
-		# Send data length
-		clientsocket.sendall(struct.pack("Q", len(bytes_data)))
-		print(len(bytes_data))
+	def send_data(self):
+		jpg_as_text = self.capture_image()
+		self.read_sensor_data()
 		
-		# Send data
-		clientsocket.sendall(bytes_data)
+		data = {"image": jpg_as_text, "params": self.sensor_data}
 
-	except Exception as e:
-		print(f"Error experienced: {e}")
-		
-	Timer(5, background_controller).start()
+		json_data = json.dumps(data)
+		bytes_data = json_data.encode()
 
-while True:
-		clientsocket, address = s.accept()
-		print(f"Connection from {address} has been established.")
-		ser = serial.Serial('/dev/tty.usbserial-0001', 9600, timeout=1)
-		ser.reset_input_buffer()
-		background_controller()
+		try:
+			self.client_socket.sendall(struct.pack("Q", len(bytes_data)))
+			self.client_socket.sendall(bytes_data)
+		except Exception as e:
+			print(f"Error experienced: {e}")
+
+	def run(self):
+		while True:
+			self.client_socket, address = self.server_socket.accept()
+			print(f"Connection from {address} has been established.")
+			self.receive_data()
+
+
+# Usage
+IP = '192.168.1.158'
+PORT = 5010
+server = TransmitServer(IP, PORT, emulate_data=True)
+server.run()
